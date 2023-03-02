106e81bb242a2afdf14aec5abb88c11ed12bb078
doriszh0 <doodledodo8@gmail.com>
2023-03-02 12:07:36 -0800

    saxs

--- grade.sh
+++ grade.sh
@@ -1,10 +1,11 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 mkdir student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cp TestListExamples.java student-submission
 cd student-submission
 if [[ -f ListExamples.java ]]
 then 
@@ -14,9 +15,7 @@ else
     exit 1
 fi
 
-cp TestListExamples.java student-submission
-
-javac -cp $CPATH TestListExamples.java ListExamples.java
+javac -cp $CPATH *.java 
 if [[ $? -eq 0 ]];
 then 
     echo "ListExamples.java successfully compiled"
@@ -25,4 +24,19 @@ else
     exit 1
 fi
 
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > score.txt
+
+grep -C 0 "Tests run:" score.txt > grep-score.txt
+if [[ -s grep-score.txt ]]
+then
+    grep -o "[0-9]*" grep-score.txt > numbers.txt
+    TESTS=`head -n 1 numbers.txt`
+    FAILS=`tail -n 1 numbers.txt`
+    DIFFERENCE=`expr $TESTS - $FAILS`
+    GRADE=`expr $DIFFERENCE / $TESTS \* 100`
+    echo "You failed $FAILS out of $TESTS tests"
+    echo "Your grade is $GRADE%"
+else 
+    echo "You failed 0 tests"
+    echo "Your grade is 100%!"
+fi
